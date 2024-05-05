13aabfa216391557813856ce6da2c36351595de3
김형민 <rlagudals95@gimhyeongmin-ui-MacBookAir.local>
2024-05-05 21:31:36 +0900

    feat: tutorial

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,25 @@
 import type { FunctionComponent } from "../common/types";
+import { useCallback, useEffect } from "react";
+import { useNavigate } from "@tanstack/react-router";
 
 export const Home = (): FunctionComponent => {
+
+	const navigate = useNavigate({})
+
+	const checkAuth = useCallback(async() => {
+	
+		//@TODO - check login
+		await navigate({ to: '/tutorial' })	
+	
+	} ,[navigate])
+
+	useEffect(() => {
+		checkAuth()
+			.catch((error) => {
+				throw error
+			});
+	} ,[checkAuth])
+
 	return (
 		<div className="bg-blue-300  font-bold w-screen h-screen flex flex-col justify-center items-center ">
 			<p className="text-white text-6xl">Hello, world!</p>
